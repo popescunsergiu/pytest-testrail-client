7fc3ed7cdd702dfd3efab7fce0d89937ee9ee195
Sergiu Popescu <sergiu@moduscreate.com>
2020-05-14 22:22:40 +0300

    Add version 0.1.0

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest_testrail",
+    version='0.1.0',
     use_scm_version=True,
     description="pytest plugin for Testrail",
     long_description=open("README.rst").read(),
